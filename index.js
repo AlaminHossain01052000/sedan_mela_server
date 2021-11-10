8190c5999b3e936f428a208b9c5bf08e55a7b063
Al Amin <alaminhossain2000and@gmail.com>
2021-11-11 04:33:37 +0600

    work with purchase

--- index.js
+++ index.js
@@ -3,6 +3,7 @@ const app = express();
 const { MongoClient } = require('mongodb');
 const port = process.env.location || 5000;
 const cors = require('cors');
+const ObjectId = require('mongodb').ObjectId;
 require("dotenv").config();
 
 // middleware
@@ -16,6 +17,7 @@ async function run() {
         const sedanCollection = client.db("sedan_mela").collection("ourSedans");
         const testimonialCollection = client.db("sedan_mela").collection("testimonials");
         const userCollection = client.db("sedan_mela").collection("users");
+        const purchasedSedanCollection = client.db("sedan_mela").collection("purchasedSedan");
         app.get("/sedans", async (req, res) => {
             const sedans = await sedanCollection.find({}).toArray();
             res.json(sedans);
@@ -23,6 +25,19 @@ async function run() {
         app.get("/testimonials", async (req, res) => {
             const testimonials = await testimonialCollection.find({}).toArray();
             res.json(testimonials);
+        })
+        app.get("/sedan/:id", async (req, res) => {
+            const id = req.params.id;
+            const query = { _id: ObjectId(id) };
+            const result = await sedanCollection.findOne(query);
+            res.json(result);
+        })
+        app.get("/purchasedSedan", async (req, res) => {
+
+            const email = req.query.email;
+            const query = { email: email };
+            const result = await purchasedSedanCollection.find(query).toArray();
+            res.json(result);
         })
         app.post("/users", async (req, res) => {
 
@@ -34,6 +49,11 @@ async function run() {
             const options = { upsert: true }
             const user = { $set: req.body };
             const result = await userCollection.updateOne(filter, user, options);
+            res.json(result);
+        })
+        app.post('/purchasedSedan', async (req, res) => {
+            const body = req.body;
+            const result = await purchasedSedanCollection.insertOne(body);
             res.json(result);
         })
     }
