9dc82f3e43d11cbae1d49f5d688ef57921e6cafa
Al Amin <alaminhossain2000and@gmail.com>
2021-11-10 22:11:21 +0600

    sedan collection and testimonial collection set

---
+++ index.js
@@ -0,0 +1,39 @@
+const express = require('express');
+const app = express();
+const { MongoClient } = require('mongodb');
+const port = process.env.location || 5000;
+const cors = require('cors');
+require("dotenv").config();
+
+// middleware
+app.use(cors());
+app.use(express.json());
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.li11u.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`;
+const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true });
+async function run() {
+    try {
+        await client.connect();
+        const sedanCollection = client.db("sedan_mela").collection("ourSedans");
+        const testimonialCollection = client.db("sedan_mela").collection("testimonials");
+        app.get("/sedans", async (req, res) => {
+            const sedans = await sedanCollection.find({}).toArray();
+            res.json(sedans);
+        })
+        app.get("/testimonials", async (req, res) => {
+            const testimonials = await testimonialCollection.find({}).toArray();
+            res.json(testimonials);
+        })
+    }
+    finally {
+
+    }
+}
+run().catch(console.dir);
+
+app.get("/", (req, res) => {
+    console.log("backend is working fine")
+    res.json("Backend is working");
+})
+app.listen(port, () => {
+    console.log("Listening to port ", port);
+})
