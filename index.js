3abd49e4d298893bfde2266997bf9343fb6a387f
Al Amin <alaminhossain2000and@gmail.com>
2021-11-12 15:58:45 +0600

    port path changing

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const app = express();
 const { MongoClient } = require('mongodb');
-const port = process.env.location || 5000;
+const port = process.env.PORT || 5000;
 const cors = require('cors');
 const ObjectId = require('mongodb').ObjectId;
 require("dotenv").config();
