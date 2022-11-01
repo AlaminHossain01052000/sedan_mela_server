9d299084c2a5e534a0589428923223c271f493ba
Al Amin <alaminhossain2000and@gmail.com>
2022-11-01 21:46:08 +0600

    authentication issus is resolved

--- index.js
+++ index.js
@@ -11,7 +11,7 @@ app.use(cors());
 app.use(express.json());
 
 // connecting node js with mongodb
-const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.li11u.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`;
+const uri = `mongodb+srv://sedanMela:yOovhds1Bqmo8rUz@cluster0.li11u.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`;
 const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true });
 async function run() {
     try {
